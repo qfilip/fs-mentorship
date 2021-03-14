8a24e296246a7de561b4454400dc791626ee3a72
qfilip <kukolj.filip@gmail.com>
2021-03-14 20:32:49 +0100

    MODIFY: Server Side app with Giraffe

---
+++ Server_Sol/Server.Api/Program.fs
@@ -0,0 +1,74 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open Giraffe
+open Microsoft.Extensions.Logging
+open Microsoft.Extensions.Hosting
+open System.IO
+open Microsoft.AspNetCore.Cors.Infrastructure
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
+open Microsoft.Extensions.DependencyInjection
+
+let webApp =
+    choose [
+        GET >=>
+            choose [
+                route "/" >=> text "Hi"
+                route "/hello" >=> text "Hi"
+            ]
+        setStatusCode 404 >=> text "Not Found" ]
+
+let errorHandler (ex : Exception) (logger : ILogger) =
+    logger.LogError(ex, "An unhandled exception has occurred while executing the request.")
+    clearResponse >=> setStatusCode 500 >=> text ex.Message
+
+
+let configureCors (builder : CorsPolicyBuilder) =
+    builder
+       .WithOrigins("http://localhost:5500")
+       .AllowAnyMethod()
+       .AllowAnyHeader()
+       |> ignore
+
+
+let configureApp (app : IApplicationBuilder) =
+    let env = app.ApplicationServices.GetService<IWebHostEnvironment>()
+    (match env.IsDevelopment() with
+    | true  ->
+        app.UseDeveloperExceptionPage()
+    | false ->
+        app .UseGiraffeErrorHandler(errorHandler)
+            .UseHttpsRedirection())
+        .UseCors(configureCors)
+        .UseStaticFiles()
+        .UseGiraffe(webApp)
+
+
+let configureServices (services : IServiceCollection) =
+    services.AddCors()    |> ignore
+    services.AddGiraffe() |> ignore
+
+
+let configureLogging (builder : ILoggingBuilder) =
+    builder.AddConsole()
+           .AddDebug() |> ignore
+
+
+[<EntryPoint>]
+let main args =
+    let contentRoot = Directory.GetCurrentDirectory()
+    let webRoot     = Path.Combine(contentRoot, "WebRoot")
+    Host.CreateDefaultBuilder(args)
+        .ConfigureWebHostDefaults(
+            fun webHostBuilder ->
+                webHostBuilder
+                    // .UseContentRoot(contentRoot)
+                    // .UseWebRoot(webRoot)
+                    .Configure(Action<IApplicationBuilder> configureApp)
+                    .ConfigureServices(configureServices)
+                    .ConfigureLogging(configureLogging)
+                    |> ignore)
+        .Build()
+        .Run()
+    0
