62b99b17d85ce368d3693110c08753e0ee34af80
qfilip <kukolj.filip@gmail.com>
2021-04-01 21:13:40 +0200

    ADD: asyncSample.fsx

--- Server_Sol/Server.Api/Dtos.fs
+++ Server_Sol/Server.Api/Dtos.fs
@@ -1,10 +1,11 @@
 module Dtos
 open System
+open DomainModels.Common
 open SimpleTypes.Strength
-open DomainModels
 
 
 module Audit = 
+    [<CLIMutable>]
     type AuditDto = {
         Id: string
         EntityId: string
@@ -13,7 +14,9 @@ module Audit =
     }
 
 
+
 module Excerpt =
+    [<CLIMutable>]
     type ExcerptDto = {
         Id: Guid
         Name: string
@@ -21,20 +24,32 @@ module Excerpt =
     }
 
 
+
 open Excerpt
 
 module Ingredient =
+    [<CLIMutable>]
     type IngredientDto = {
         Id: Guid
         Name: string
         Strength: Strength
 
         Excerpts: ExcerptDto list option
+    } 
+    and CocktailDto = {
+       Id: Guid
+       AuditId: Guid
+
+       Name: string
+       Description: string option
+       
+       Excerpts: ExcerptDto list option
     }
 
 
-module Cocktail =
 
+module Cocktail =
+    [<CLIMutable>]
     type CocktailDto = {
         Id: Guid
         AuditId: Guid
