9978573342428f204c5cb0f3ec4cdfe52a6de44f
qfilip <kukolj.filip@gmail.com>
2021-05-04 02:11:09 +0200

    UPDATE: continued work on project

--- Simple_Sol/EvenSimpler/BusinessLogic.fs
+++ Simple_Sol/EvenSimpler/BusinessLogic.fs
@@ -4,18 +4,38 @@ open Dal.Common
 open ValidationCommon
 open Models.Dtos
 
-let checkUserExists (user: UserDto) =
-    let findUser (db: SampleDatabase) = 
-        db.Users |> List.tryFind (fun x -> x.Key = user.Key)
-    
-    let dbResult = (execute findUser)
+let onDbResult f dbResult = 
     match dbResult with
-    | Failure fs -> fs
-    | Success res ->
-        match res with
-        | None -> [""]
-        | Some u -> [""] // ??? How to return true/false or list of errors
+    | Failure _ -> failwith "Database error"
+    | Success result -> f result
 
 
-let executeTransaction (x: TransactionDto) =
+let checkUserExists (userKey: int) =
+    let command (db: SampleDatabase) = 
+        db.Users |> List.tryFind (fun x -> x.Key = userKey)
+    
+    let dbResult = (execute command)
+    
+    let parseResult x = function
+    | None -> false
+    | Some _ -> true
+    
+    (onDbResult parseResult dbResult)
+
+
+let getUsersByKeys (keys: int list) =
+    let command (db: SampleDatabase) = 
+        db.Users |> List.filter (fun x -> keys |> List.contains x.Key)
+
+    let dbResult = execute command
+    let parseResult x = Success x
+
+    (onDbResult parseResult dbResult)
+
+
+let validateTransactionRequest (dto: TransactionDto) =
+    let keys = [dto.SenderKey; dto.RecieverKey]
+    let users = getUsersByKeys keys
+        
+
     0
